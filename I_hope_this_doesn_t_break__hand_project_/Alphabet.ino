3c84d39745ec79f627ed716fec56f1f822a91ccd
evilparsnips <36208390+evilparsnips@users.noreply.github.com>
2018-03-06 19:04:31 +0000

    Alphabet complete
    Needs modifications and fine tuning; notes included in code

--- I_hope_this_doesn_t_break__hand_project_/Alphabet.ino
+++ I_hope_this_doesn_t_break__hand_project_/Alphabet.ino
@@ -1,14 +1,66 @@
 void A (){
-  fingermove(0, 9);
+  fingermove(0, 8);
   fingermove(1, 0);
   fingermove(2, 0);
   fingermove(3, 0);
   fingermove(4, 0);
 }
 
+void B () {
+  fingermove(0, 0);
+  fingermove(1, 9);
+  fingermove(2, 9);
+  fingermove(3, 9);
+  fingermove(4, 9);
+}
+
+void C () {
+  fingermove(0, 2);
+  fingermove(1, 3);
+  fingermove(2, 3);
+  fingermove(3, 3);
+  fingermove(4, 3);
+}
+
+void D () {
+  fingermove(1, 9);
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+}
+
 void E () {
   fingermove(1, 0);
   fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+}
+
+void F () {
+  fingermove(1, 0);
+  fingermove(2, 9);
+  fingermove(3, 9);
+  fingermove(4, 9);
+  wait(2);
+  fingermove(0, 0);
+}
+
+void G () {
+  fingermove(1, 9);
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+} //Same as D, except requires wrist movement
+
+void H () {
+  fingermove(1, 9);
+  fingermove(2, 9);
   fingermove(3, 0);
   fingermove(4, 0);
   wait(2);
@@ -24,6 +76,52 @@ void I () {
   fingermove(0, 0);
 }
 
+void J () {
+  fingermove(1, 0);
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 9);
+  wait(2);
+  fingermove(0, 0);
+} //Same as I, but requires wrist movement
+
+void K () {
+  fingermove(1, 9);
+  fingermove(2, 9);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+} //Similar to H, wrist movement + sideways movement required
+
+void L () {
+  fingermove(1, 9);
+  fingermove(2, 9);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+}
+
+void M () {
+  fingermove(0, 0);
+  wait(2);
+  fingermove(1, 0);
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 0); 
+}
+
+void N () {
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 2);
+  wait(2);
+  fingermove(1, 2);
+  fingermove(2, 2);
+} //Needs fine tuning, may destroy the hand >.>
+
 void O () {
   fingermove(1, 4);
   fingermove(2, 4);
@@ -32,6 +130,50 @@ void O () {
   wait(2);
   fingermove(0, 4);  
 }
+
+void P () {
+  fingermove(0, 7);
+  fingermove(2, 3);
+  fingermove(3, 3);
+  fingermove(4, 3);
+  fingermove(1, 9);
+} //Needs wrist movement
+
+void Q () {
+  fingermove(0, 7);
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  fingermove(1, 9);
+} //Needs wrist movement
+
+void R () {
+  fingermove(1, 9);
+  fingermove(2, 9);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+} //Needs sideways movement
+
+void S () {
+  fingermove(1, 0);
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 3);
+}
+
+void T () {
+  fingermove(3, 0);
+  fingermove(4, 0);
+  fingermove(2, 0);
+  wait(2);
+  fingermove(0, 4);
+  wait(2);
+  fingermove(1, 2);
+} //Needs fine tuning, may destroy hand :)
 
 void U () {
   fingermove(1, 9);
@@ -42,5 +184,47 @@ void U () {
   fingermove(0, 0);
 }
 
+void V () {
+  fingermove(1, 9);
+  fingermove(2, 9);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+} //Similar to U, needs sideways movement
 
-  
+void W () {
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 3);
+  fingermove(1, 9);
+  fingermove(2, 9);
+  fingermove(3, 9);
+}
+
+void X () {
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+  fingermove(1, 7);
+}
+
+void Y () {
+  fingermove(0, 9);
+  fingermove(1, 0);
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 9);  
+}
+
+void Z () {
+  fingermove(1, 9);
+  fingermove(2, 0);
+  fingermove(3, 0);
+  fingermove(4, 0);
+  wait(2);
+  fingermove(0, 0);
+} //Needs complex movement
+
